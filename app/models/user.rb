1a74108b7abd9b303652def34a81410b81d4c66a
Ashish Malhotra <ash6320@gmail.com>
2012-05-27 19:56:40 +0300

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
   
+  has_many :microposts, dependent: :destroy
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
   validates :name, presence: true, length: { maximum: 50 }
@@ -22,6 +23,11 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+  
+  def feed
+    # This is preliminary.....
+    Micropost.where("user_id = ?", id)
+  end
   
   private
     def create_remember_token
